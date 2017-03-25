d49d685a6c25975451120959f2161ed9821b4672
lvziwen-bst <lvziwen0201@gmail.com>
2017-03-25 17:04:43 +0800

    change config file for myself

--- config.default.js
+++ config.default.js
@@ -10,20 +10,21 @@ var config = {
 
   get mini_assets() { return !this.debug; }, // 是否启用静态文件的合并压缩，详见视图中的Loader
 
-  name: 'Nodeclub', // 社区名字
-  description: 'CNode：Node.js专业中文社区', // 社区的描述
-  keywords: 'nodejs, node, express, connect, socket.io',
+  name: 'Hi-XiangHe', // 社区名字
+  description: 'hi-xianghe汇聚网络最优惠，让生活更美好', // 社区的描述
+  keywords: '香河,xianghe,优惠,优惠券,淘宝',
 
   // 添加到 html head 中的信息
   site_headers: [
-    '<meta name="author" content="EDP@TAOBAO" />'
+    '<meta name="author" content="lvziwen" />'
   ],
   site_logo: '/public/images/cnodejs_light.svg', // default is `name`
   site_icon: '/public/images/cnode_icon_32.png', // 默认没有 favicon, 这里填写网址
+  myqrcode: '/public/images/myqrcode.png', // 默认没有 favicon, 这里填写网址
   // 右上角的导航区
   site_navs: [
     // 格式 [ path, title, [target=''] ]
-    [ '/about', '关于' ]
+    // [ '/about', '关于' ]
   ],
   // cdn host，如 http://cnodejs.qiniudn.com
   site_static_host: '', // 静态文件存储域名
@@ -41,13 +42,13 @@ var config = {
   redis_host: '127.0.0.1',
   redis_port: 6379,
   redis_db: 0,
-  redis_password: '',
+  redis_password: ':lvziwen',
 
-  session_secret: 'node_club_secret', // 务必修改
-  auth_cookie_name: 'node_club',
+  session_secret: 'hi-xianghe-8686', // 务必修改
+  auth_cookie_name: 'hi-xianghe',
 
   // 程序运行的端口
-  port: 3000,
+  port: 80,
 
   // 话题列表显示的话题数量
   list_topic_count: 20,
@@ -64,11 +65,11 @@ var config = {
 
   // 邮箱配置
   mail_opts: {
-    host: 'smtp.126.com',
+    host: 'smtp.163.com',
     port: 25,
     auth: {
-      user: 'club@126.com',
-      pass: 'club'
+      user: 'lvziwen0201@163.com',
+      pass: 'yanghuifang8686'
     },
     ignoreTLS: true,
   },
@@ -78,7 +79,7 @@ var config = {
   weibo_id: 'your_weibo_id',
 
   // admin 可删除话题，编辑标签。把 user_login_name 换成你的登录名
-  admins: { user_login_name: true },
+  admins: { "hayao650": true },
 
   // github 登陆的配置
   GITHUB_OAUTH: {
@@ -116,9 +117,16 @@ var config = {
 
   // 版块
   tabs: [
-    ['share', '分享'],
-    ['ask', '问答'],
-    ['job', '招聘'],
+    // ['good',         '精选'    ],
+    ['lady',         '女装'    ],
+    ['man',          '男装'    ],
+    ['make_up',      '美妆个护'],
+    ['shoes_bag',    '鞋包配饰'],
+    ['home',         '家居家装'],
+    ['underclothes', '内衣'    ],
+    ['digit',        '数码家电'],
+    ['mom_baby',     '母婴'    ],
+    ['outdoor',      '户外运动'],
   ],
 
   // 极光推送
