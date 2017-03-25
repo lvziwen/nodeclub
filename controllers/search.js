e7f4c27b61f11288b4107f36b96f187dec1b9905
lvziwen-bst <lvziwen0201@gmail.com>
2017-03-25 16:59:59 +0800

    first update

--- controllers/search.js
+++ controllers/search.js
@@ -1,5 +1,6 @@
 exports.index = function (req, res, next) {
   var q = req.query.q;
   q = encodeURIComponent(q);
-  res.redirect('https://www.google.com.hk/#hl=zh-CN&q=site:cnodejs.org+' + q);
+  // res.redirect('https://www.baidu.com/#hl=zh-CN&q=site:hi-xianghe.com+' + q);
+  res.redirect('https://www.baidu.com/#hl=zh-CN&wd=' + q + " site:hi-xianghe.com");
 };
